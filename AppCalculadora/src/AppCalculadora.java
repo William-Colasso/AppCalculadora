16851ee12037b638b291899aa5d5792793acb172
William Colasso <161973026+William-Colasso@users.noreply.github.com>
2025-08-26 16:18:17 -0300

    Teste das novas funcionalidades
    Testes das funcionalidades soma e subtração, pelo console.

--- AppCalculadora/src/AppCalculadora.java
+++ AppCalculadora/src/AppCalculadora.java
@@ -1,5 +1,12 @@
 public class AppCalculadora {
     public static void main(String[] args) throws Exception {
         Calculadora calculadora = new Calculadora();
+
+
+        //Teste da soma 
+        System.out.println("Soma: "+calculadora.soma(5,5));
+
+        //Teste da subtração
+        System.out.println("Subtração: "+calculadora.subtracao(5, 5));
     }
 }
