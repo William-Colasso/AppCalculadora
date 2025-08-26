a76311a4c0d36c6b282322fc1c503f382b72aec2
Vinicius Pedro Andreazza <154294270+vinicius-andreazza@users.noreply.github.com>
2025-08-26 16:26:36 -0300

    Implementação de testes
    Implementação de testes das funções multiplicação e divisão

--- AppCalculadora/src/AppCalculadora.java
+++ AppCalculadora/src/AppCalculadora.java
@@ -1,5 +1,11 @@
 public class AppCalculadora {
     public static void main(String[] args) throws Exception {
         Calculadora calculadora = new Calculadora();
+
+        //Teste divisão
+        System.out.println("Divisão: " + calculadora.divisao(10, 5));
+        //Teste multiplicação
+        System.out.println("Multiplicação: " + calculadora.multiplicacao(10, 5));
+        
     }
 }
